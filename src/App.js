29e20887de85d180afd3b9006fc0a9faca389606
alfinroy05 <alfinroy2005@gmail.com>
2024-10-10 00:58:04 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Index from './Components/Index';
+import Add from './Components/Add';
+import Delete from './Components/Delete';
+import Search from './Components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Index/>}/>
+      <Route path='/add' element={<Add/>}/>
+      <Route path='/delete' element={<Delete/>}/>
+      <Route path='/search' element={<Search/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
